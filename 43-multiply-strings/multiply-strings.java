9333a879773da94c89f2a77dc8eb8b414098e0fd
Pratik Kumar <pratikkumar232005@gmail.com>
2026-06-12 11:42:31 +0530

    Time: 3 ms (81.05%) | Memory: 43.6 MB (66.10%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+
+        int m = num1.length();
+        int n = num2.length();
+
+        int[] result = new int[m + n];
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+
+                int p1 = i + j;
+                int p2 = i + j + 1;
+
+                int sum = mul + result[p2];
+
+                result[p2] = sum % 10;
+                result[p1] += sum / 10;
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int num : result) {
+            if (!(ans.length() == 0 && num == 0)) {
+                ans.append(num);
+            }
+        }
+
+        return ans.toString();
+    }
+}
