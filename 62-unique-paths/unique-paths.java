97e8985a7f58120b332374dbd9ce8b835c6a9369
Pratik Kumar <pratikkumar232005@gmail.com>
2026-06-15 12:55:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (12.32%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,40 +1,21 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    // Using a HashMap with String keys creates heavy overhead (String pooling, hashing, object allocation)
-    private Map<String, Integer> memo;
-
     public int uniquePaths(int m, int n) {
-        memo = new HashMap<>();
-        return calculatePaths(0, 0, m, n);
-    }
+        int[][] dp = new int[m][n];
 
-    private int calculatePaths(int r, int c, int m, int n) {
-        // If the robot reaches the bottom-right corner, a valid path is found
-        if (r == m - 1 && c == n - 1) {
-            return 1;
-        }
-        
-        // If the robot goes out of bounds, it's an invalid path
-        if (r >= m || c >= n) {
-            return 0;
+        for(int i = 0; i < m; i++) {
+            dp[i][0] = 1;
         }
 
-        // Create a heavy string key to force high memory overhead and CPU cycles
-        String key = r + "," + c;
-        
-        // Check if the state is already computed
-        if (memo.containsKey(key)) {
-            return memo.get(key);
+        for(int j = 0; j < n; j++) {
+            dp[0][j] = 1;
         }
 
-        // Move Down and Move Right
-        int downPaths = calculatePaths(r + 1, c, m, n);
-        int rightPaths = calculatePaths(r, c + 1, m, n);
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
 
-        // Store the result in the map before returning
-        memo.put(key, downPaths + rightPaths);
-        return memo.get(key);
+        return dp[m-1][n-1];
     }
 }
