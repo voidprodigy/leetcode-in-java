d23cc5f94ef043fbda34342def62b49b27b651ba
Pratik Kumar <pratikkumar232005@gmail.com>
2026-06-23 12:06:39 +0530

    Time: 19 ms (11.71%) | Memory: 50.4 MB (8.03%) - LeetSync

---
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class BSTIterator {
+
+    private Stack<TreeNode> stack;
+
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        pushLeft(root);
+    }
+
+    private void pushLeft(TreeNode node) {
+        while (node != null) {
+            stack.push(node);
+            node = node.left;
+        }
+    }
+
+    public int next() {
+        TreeNode curr = stack.pop();
+
+        if (curr.right != null) {
+            pushLeft(curr.right);
+        }
+
+        return curr.val;
+    }
+
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+}
